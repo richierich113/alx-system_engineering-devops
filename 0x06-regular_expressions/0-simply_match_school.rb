efbce84241a5905f9236bc6300c292c0a2ed6bc3
Richard Boakye Duah <richrichie784@gmail.com>
2023-10-03 18:55:02 -0700

    Creates 0-simply_match_school.rb

---
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/env ruby
+#  Ruby script that accepts one argument and pass it to a regular expression
+# matching method
+# The regular expression must match School
+
+puts ARGV[0].scan(/School/).join
---
+++ 0x06-regular_expressions/README.md
@@ -0,0 +1,2 @@
+# 1-repetition_token_0.rb
+This directory covers projects on regular expression using Oniguruma, a regular expression library that which is used by Ruby by default. 
