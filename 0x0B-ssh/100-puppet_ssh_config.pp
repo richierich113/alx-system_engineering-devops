0811f56cb263fd62c383b0add5233b03ddb71ee0
Richard Duah Boakye <richrichie784@gmail.com>
2024-03-25 05:56:25 +0000

    Update 100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,22 +1,19 @@
-# Let’s practice using Puppet to make changes to our configuration file.
-# Just as in the previous configuration file task, we’d like you to set
-# up your client SSH configuration file so that you can connect to a server
-# without typing a password.
+# Puppet manifest to configure SSH client
 
-# Requirements:
-#     Your SSH client configuration must be configured to use the private key `~/.ssh/school`
-#     Your SSH client configuration must be configured to refuse to authenticate using a password
-
-
-# Turn off passwd auth
-exec {'Turn off passwd auth':
-  command  => 'echo "    PasswordAuthentication no" >> /etc/ssh/ssh_config',
-  provider => 'shell'
+# Ensure SSH client configuration file exists
+file { '/home/your_username/.ssh/config':
+  ensure => file,
 }
 
-# Declare identity file
-exec {'Declare identity file':
-  require  => Exec['Turn off passwd auth'],
-  command  => 'echo "    IdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config',
-  provider => 'shell'
+# Set up SSH client configuration
+file_line { 'Turn off passwd auth':
+  path   => '/home/your_username/.ssh/config',
+  line   => 'PasswordAuthentication no',
+  match  => '^#?PasswordAuthentication',
+}
+
+file_line { 'Declare identity file':
+  path   => '/home/your_username/.ssh/config',
+  line   => 'IdentityFile ~/.ssh/school',
+  match  => '^#?IdentityFile',
 }
